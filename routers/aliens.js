298d0c418c1e4fa46ae77f438c1aefbb0b8a48c2
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-07 18:00:35 +0530

    creating schema

--- routers/aliens.js
+++ routers/aliens.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const Alien = require('../models/alien')
+
 router.get('/', async(req, res) => {
     try{
-        const aliens = 
-
+        const aliens = Alien.find()
     }catch(err){
         res.send('Error' + err)
     }
